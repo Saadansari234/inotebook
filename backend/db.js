cfae865c5233b69fc848490a6abab2d3ef16becf
Saadansari234 <saadansari3212@gmail.com>
2024-08-06 23:22:57 +0530

    video-43 connecting mongo and express to node js app

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose= require("mongoose")
+
+const mongoURL="mongodb://localhost:27017/?directConnection=true"
+
+const  ConnectToMongo= async()=>{
+    try {
+        await mongoose.connect(mongoURL);
+        console.log('Database connection successful');
+      } catch (error) {
+        console.error('Database connection error:', error);
+      }
+}
+
+module.exports =  ConnectToMongo
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+const ConnectToMongo= require("./db")
+const express = require('express')
+
+ConnectToMongo()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
