99fd2fa610e9d694fbe7892b98ee1d0494f7b861
Saadansari234 <saadansari3212@gmail.com>
2024-08-08 08:54:01 +0530

    video-47 Creating ThunderClient Collections to Manage Requests

--- backend/models/User.js
+++ backend/models/User.js
@@ -23,8 +23,6 @@ const UserSchema= new Schema({
 })
 
 const User= mongoose.model('user', UserSchema )
-// its to verify if the sended data is already existed in mongo
-User.createIndexes()
 
 module.exports=  User
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,29 +3,38 @@ const { validationResult, body } = require('express-validator')
 const User = require("../models/User")
 const router = express.Router()
 
-//create a user using post 
-router.post('/', [
+//create a user using post using /api/auth/createuser
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Enter a valid password').isLength({ min: 5 }),
-], (req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-        return res.status(400).send({ errors: result.array() });
-    }
+], async (req, res) => {
+    try {
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password // Ensure you hash the password before saving in a real application
-    })
-        .then(user => res.json(user))
-        .catch(err => {
-            console.log(err)
-            res.json({ error: "enter a unique value for email", message: err.message })
+        // if there are errors return bad request and error
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).send({ errors: result.array() });
+        }
+        // check if the user is already exist
+        let user = await User.findOne({ email: req.body.email })
+
+        if (user) {
+            return res.status(400).json({ error: "user with this emil already exist" })
+        }
+        // reating new user if the user not already existed 
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password // Ensure you hash the password before saving in a real application
         })
+        
+        res.json(user)
 
-
+    } catch (error) {
+     console.error(error.message)
+     res.status(500).send("some error occured")
+    }
 });
 
 module.exports = router
