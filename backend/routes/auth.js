2b55266c74699417f93f102b98eedb924f718fb5
Saadansari234 <saadansari3212@gmail.com>
2024-08-08 20:40:11 +0530

    video-49 Hashing Passwords using bcryptjs in NodeJs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const bcrypt = require('bcryptjs');
 const { validationResult, body } = require('express-validator')
 const User = require("../models/User")
 const router = express.Router()
+const jwt= require('jsonwebtoken')
+const JWT_secret= 'saadisagoodb$oy'
 
 //create a user using post using /api/auth/createuser
 router.post('/createuser', [
@@ -22,14 +25,27 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "user with this emil already exist" })
         }
+
+        // hash the password before saving in a real application
+        const salt = await bcrypt.genSaltSync(10);
+        const securePassword = await bcrypt.hashSync( req.body.password , salt);
         // reating new user if the user not already existed 
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password // Ensure you hash the password before saving in a real application
+            password:securePassword // Ensure you hash the password before saving in a real application
         })
-        
-        res.json(user)
+
+        // 
+        const data= {
+            user:{
+                id: user.id
+            }
+        }
+        const AuthToken= jwt.sign(data, JWT_secret)
+        console.log(AuthToken);
+        res.json({AuthToken})
+        // res.json(user)
 
     } catch (error) {
      console.error(error.message)
